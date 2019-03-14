e46881e425ebceffe4216c103d69f074e84f42f5
Myeishamadkins <mmadkins@basecamecodingacademy.org>
2019-03-14 09:12:37 -0500

    Changes to forms.py:
    Added a application form that will take in the name, email, number,
    and a short note to the company from the user/person applying for the
    job.

--- app/forms.py
+++ app/forms.py
@@ -5,9 +5,19 @@ class PostJobForm(forms.Form):
     companyName = forms.CharField(label="Company Name:")
     jobTitle = forms.CharField(label="Job Title:")
     jobLocation = forms.CharField(label="Job Location:")
-    employmentType = forms.CharField(label="Employment Type")
-    seniorityLevel = forms.CharField(label="Seniority Level")
+    employmentType = forms.CharField(label="Employment Type:")
+    seniorityLevel = forms.CharField(label="Seniority Level:")
     jobDescription = forms.CharField(
         label="Job Description",
         widget=forms.Textarea(
             attrs={'placeholder': 'Enter your job description here'}))
+
+
+class ApplicationForm(forms.Form):
+    name = forms.CharField(label='Name:')
+    email = forms.EmailField(label='Email:')
+    phoneNumber = forms.CharField(label='Phone Number:')
+    companyNote = forms.CharField(
+        label='Note to Company:',
+        widget=forms.Textarea(
+            attrs={'placeholder': 'Enter your note here...'}))
