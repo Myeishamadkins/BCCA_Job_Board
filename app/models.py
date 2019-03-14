e8c20920262e429d03f63e606189f1cbad7ea733
Myeishamadkins <mmadkins@basecamecodingacademy.org>
2019-03-14 09:16:06 -0500

    Changes to models.py:
    added 2 classes, PostJob and Application.
    
    The PostJob class takes in the company name, job title, job location,
    employment type, seniority level, and job description from the
    employer and formats it in the data base.
    
    The Application class takes i nthe name, email, phone number, and
    the short note to the company that the applicant has given and formats
    it in the data base.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+
 # Create your models here.
+
+
+class PostJob(models.Model):
+    companyName = models.TextField()
+    jobTitle = models.TextField()
+    jobLocation = models.TextField()
+    employmentType = models.TextField()
+    seniorityLevel = models.TextField()
+    jobDescription = models.TextField()
+
+    def __str__(self):
+        return '''
+        Company Name: {}
+        Job Title: {}
+        Job Location: {}
+        Employment Type: {}
+        Seniority Level: {}
+        Job Description: {}
+        '''.format(self.companyName, self.jobTitle, self.jobLocation,
+                   self.employmentType, self.seniorityLevel,
+                   self.jobDescription)
+
+    @staticmethod
+    def submit_job(companyName, jobTitle, jobLocation, employmentType,
+                   seniorityLevel, jobDescription):
+        PostJob(
+            companyName=companyName,
+            jobTitle=jobTitle,
+            jobLocation=jobLocation,
+            employmentType=employmentType,
+            seniorityLevel=seniorityLevel,
+            jobDescription=jobDescription).save()
+
+
+class Application(models.Model):
+    name = models.TextField()
+    email = models.EmailField()
+    phoneNumber = models.TextField()
+    companyNote = models.TextField()
+    post_job = models.ForeignKey(PostJob, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return '''
+        Name: {}
+        Email: {}
+        Phone Number: {}
+        Note to Company: {}
+        Application for Job: {}
+        '''.format(self.name, self.email, self.phoneNumber, self.companyNote,
+                   self.post_job)
+
+    @staticmethod
+    def submit_app(name, email, phoneNumber, companyNote, post_job_id):
+        Application(
+            name=name,
+            email=email,
+            phoneNumber=phoneNumber,
+            companyNote=companyNote,
+            post_job_id=post_job_id).save()
 
